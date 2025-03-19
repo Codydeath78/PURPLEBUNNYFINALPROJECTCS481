dc8553067ff820e8a5d9794a0c49b29a6f048b8f
Codydeath78 <rogeliolinares1234@gmail.com>
2025-03-19 08:35:30 -0700

    working menu navigation

--- app/src/main/java/com/example/purplebunnyteam/MainActivity.kt
+++ app/src/main/java/com/example/purplebunnyteam/MainActivity.kt
@@ -5,16 +5,59 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-//test comment
+import androidx.fragment.app.Fragment
+import com.example.purplebunnyteam.fragments.BookmarkFragment
+import com.example.purplebunnyteam.fragments.ChatFragment
+import com.example.purplebunnyteam.fragments.HomeFragment
+import com.example.purplebunnyteam.fragments.SearchFragment
+import com.example.purplebunnyteam.fragments.SettingsFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val homeFragment = HomeFragment()
+        val searchFragment = SearchFragment()
+        val chatFragment = ChatFragment()
+        val settingsFragment = SettingsFragment()
+        val bookmarkFragment = BookmarkFragment()
+
+
+        changeFragment(homeFragment)
+
+        findViewById<BottomNavigationView>(R.id.bottom_nav).setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.ic_home -> {
+                    changeFragment(homeFragment)
+                    true
+                }
+                R.id.ic_chat_bubble -> {
+                    changeFragment(chatFragment)
+                    true
+                }
+                R.id.ic_bookmark -> {
+                    changeFragment(bookmarkFragment)
+                    true
+                }
+                R.id.ic_search -> {
+                    changeFragment(searchFragment)
+                    true
+                }
+                R.id.ic_settings -> {
+                    changeFragment(settingsFragment)
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+    private fun changeFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fContainer,fragment)
+            commit()
         }
     }
 }
